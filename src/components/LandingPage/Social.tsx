c285498ae5b0e2d332ce4e65b04d50035438d39e
jasmin <chjasmin27@gmail.com>
2025-02-13 10:42:42 +0600

    social growth icon fix responsive

--- src/components/LandingPage/Social.tsx
+++ src/components/LandingPage/Social.tsx
@@ -65,8 +65,8 @@ export default function SocialSection() {
                   className="2xl:w-[600px]"
                 />
               </div>
-              <div className="absolute right-20 top-[280]">
-                <Image src={'/social-Growth.png'} width={180} height={180} alt="growth-image"/>
+              <div className="absolute lg:right-20  lg:top-[280] top-52 right-10">
+                <Image src={'/social-Growth.png'} width={150} height={180} alt="growth-image" className="lg:w-[180px]"/>
               </div>
             </div>
           </div>
