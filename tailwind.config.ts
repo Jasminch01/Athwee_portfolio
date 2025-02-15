0fad3482ee90f7cb6fa3d34cf380d6b6cb66040f
jasmin <chjasmin27@gmail.com>
2025-02-15 15:24:48 +0600

    update #1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,15 +11,16 @@ export default {
       backgroundImage: {
         "gradient-banner": "linear-gradient(120deg, #ff7e5f, #feb47b, #a67aff)",
         "mini-banner": "url('/banner.png')",
-        "footer-bg" : "linear-gradient(180deg, rgba(211, 166, 114, 0.38) -86.06%, rgba(54, 44, 32, 0.75) 48.88%, #232323 119.09%)"
+        "footer-bg":
+          "linear-gradient(180deg, rgba(211, 166, 114, 0.38) -86.06%, rgba(54, 44, 32, 0.75) 48.88%, #232323 119.09%)",
       },
       colors: {},
-      fontFamily : {
-        generalSans : ['var(--font-generalSans)'],
-        saira : ['var(--font-saira)']
+      fontFamily: {
+        generalSans: ["var(--font-generalSans)"],
+        saira: ["var(--font-saira)"],
       },
-      
     },
   },
+  safelist: ["top-[280px]", "xl:top-[280px]"],
   plugins: [],
 } satisfies Config;
